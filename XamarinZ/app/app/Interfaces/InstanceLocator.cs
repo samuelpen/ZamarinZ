408dcfa233f7222074fe6e198ffff15422d10816
Samuel Peña <samuel_pena@hotmail.es>
2019-05-18 09:27:30 -0400

    incimpleto

---
+++ XamarinZ/app/app/Interfaces/InstanceLocator.cs
@@ -0,0 +1,17 @@
+﻿namespace app.Interfaces
+{
+    using ViewModels;
+    public class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main { get; set; }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
---
+++ XamarinZ/app/app/ViewModels/LoginViewModel.cs
@@ -0,0 +1,75 @@
+﻿
+
+namespace app.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using System;
+    using System.Windows.Input;
+    public class LoginViewModel:BaseViewModel
+    {
+
+        #region Attributes
+        string email;
+        string password;
+        bool isrunning;
+        #endregion
+
+        #region Properties
+        public string Email {
+            get
+            {
+                return this.email;
+            }
+                 set
+            {
+                SetValue(ref this.email, value);
+            }
+        }
+        public string Password {
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                SetValue(ref this.password, value);
+            }
+        }
+        public bool IsRunning {
+            get
+            {
+                return this.isrunning;
+            }
+            set
+            {
+                SetValue(ref this.isrunning, value);
+            }
+        }
+
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand {
+            get
+            {
+                return new RelayCommand(cmdLogin);
+            }
+        }
+
+        private async void cmdLogin()
+        {
+            if (String.IsNullOrEmpty(Email))
+            {
+               await App.Current.MainPage.DisplayAlert("Email empty", "Please input email","Accept");
+                return;
+            }
+            if (String.IsNullOrEmpty(Password))
+            {
+                await App.Current.MainPage.DisplayAlert("Password empty", "Please input password", "Accept");
+                return;
+            }
+        }
+        #endregion
+
+    }
+}
